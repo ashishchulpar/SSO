6bf45b5b713a4bd458f4863bb98a653a9f6f2f2c
ashishchulpar <ashishchulpar@gmail.com>
2020-01-23 06:09:46 +0530

    final commit

---
+++ app1/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import com.altimetric.app1.App1Application;
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(App1Application.class, args);
+    }
+
+}
---
+++ sso-server/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import com.altimetrik.ssoserver.SsoServerApplication;
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(SsoServerApplication.class, args);
+    }
+}
